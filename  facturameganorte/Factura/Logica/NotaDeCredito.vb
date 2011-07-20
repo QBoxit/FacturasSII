8567b0e86579552b66db843ff11bd323f1a056cf
Ibarra.Alexis <Ibarra.Alexis@gmail.com>
2011-07-20 08:11:55 +0000

    Nota De credito actualizada con fecha

---  facturameganorte/Factura/Logica/NotaDeCredito.vb
+++  facturameganorte/Factura/Logica/NotaDeCredito.vb
@@ -1,175 +1,48 @@
 Public Class NotaDeCredito
 
-    Private _vendedor As String
-    Private _cliente As String
-    Private _ordenComrpra As String
-    Private _nGuiaDespacho As String
-    Private _conVenta
+    Private _id As String
+    Private _factura As String
     Private _fecha As String
-    Private _neto As Integer
-    Private _total As Integer
-    Private _iva As Integer
-    Private _persona As persona
-    Private _nnotacredito As String
 
-    Public Sub New(ByVal _vendedor As String, ByVal _cliente As String, ByVal _ordenComrpra As String, ByVal _nGuiaDespacho As String _
-    , ByVal _conVenta As String, ByVal _fecha As String, ByVal _neto As Integer, ByVal _total As Integer, ByVal _iva As Integer, _
-    ByVal _persona As persona)
+    Public Sub New(ByVal _id As String, ByVal _factura As String, ByVal _fecha As String)
 
-        Me._vendedor = _vendedor
-        Me._cliente = _cliente
-        Me._ordenComrpra = _ordenComrpra
-        Me._nGuiaDespacho = _nGuiaDespacho
-        Me._conVenta = _conVenta
+        Me._id = _id
+        Me._factura = _factura
         Me._fecha = _fecha
-        Me._neto = _neto
-        Me._total = _total
-        Me._iva = _iva
-        Me._persona = _persona
 
     End Sub
 
-    Public Sub New(ByVal _vendedor As String, ByVal _cliente As String, ByVal _ordenComrpra As String, ByVal _nGuiaDespacho As String _
-, ByVal _conVenta As String, ByVal _fecha As String, ByVal _neto As Integer, ByVal _total As Integer, ByVal _iva As Integer _
-, ByVal _nnotacredito As String)
-
-        Me._vendedor = _vendedor
-        Me._cliente = _cliente
-        Me._ordenComrpra = _ordenComrpra
-        Me._nGuiaDespacho = _nGuiaDespacho
-        Me._conVenta = _conVenta
-        Me._fecha = _fecha
-        Me._neto = _neto
-        Me._total = _total
-        Me._iva = _iva
-        Me._nnotacredito = _nnotacredito
-    End Sub
-
-    Public Property Persona() As persona
+    Public Property Id() As String
 
         Get
-            Return _persona
-        End Get
-
-        Set(ByVal value As persona)
-            _persona = value
-        End Set
-
-    End Property
-    Public Property NumeroNotaCredito() As String
-
-        Get
-            Return _nnotacredito
+            Return _id
         End Get
 
         Set(ByVal value As String)
-            _nnotacredito = value
+            _id = value
         End Set
 
     End Property
 
-    Public Property Vendedor() As String
-
+    Public Property Factura() As String
         Get
-            Return _vendedor
+            Return _factura
         End Get
 
         Set(ByVal value As String)
-            _vendedor = value
-        End Set
-
-    End Property
-
-    Public Property Cliente() As String
-        Get
-            Return _cliente
-        End Get
-
-        Set(ByVal value As String)
-            _cliente = value
-        End Set
-
-    End Property
-
-    Public Property OrdenDeCompra() As String
-
-        Get
-            Return _ordenComrpra
-        End Get
-
-        Set(ByVal value As String)
-            _ordenComrpra = value
-        End Set
-
-    End Property
-
-    Public Property NguiaDespacho() As String
-
-        Get
-            Return _nGuiaDespacho
-        End Get
-
-        Set(ByVal value As String)
-            _nGuiaDespacho = value
-        End Set
-
-    End Property
-
-    Public Property CondicionVenta() As String
-
-        Get
-            Return _conVenta
-        End Get
-
-        Set(ByVal value As String)
-            _conVenta = value
+            _factura = value
         End Set
 
     End Property
 
     Public Property Fecha() As String
+
         Get
             Return _fecha
         End Get
 
         Set(ByVal value As String)
             _fecha = value
-        End Set
-
-    End Property
-
-    Public Property Neto() As String
-
-        Get
-            Return _neto
-        End Get
-
-        Set(ByVal value As String)
-            _neto = value
-        End Set
-
-    End Property
-
-    Public Property Total() As String
-
-        Get
-            Return _total
-        End Get
-
-        Set(ByVal value As String)
-            _total = value
-        End Set
-
-    End Property
-
-    Public Property Iva() As String
-
-        Get
-            Return _iva
-        End Get
-
-        Set(ByVal value As String)
-            _iva = value
         End Set
 
     End Property
