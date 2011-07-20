a7e75f4b18e14a7a48c2ed325c3abea14986045d
Ibarra.Alexis <Ibarra.Alexis@gmail.com>
2011-07-20 08:35:12 +0000

    update notacredito con iva neto total y fecha

---  facturameganorte/Factura/Logica/NotaDeCredito.vb
+++  facturameganorte/Factura/Logica/NotaDeCredito.vb
@@ -3,12 +3,19 @@ Public Class NotaDeCredito
     Private _id As String
     Private _factura As String
     Private _fecha As String
+    Private _iva As String
+    Private _neto As String
+    Private _total As String
 
-    Public Sub New(ByVal _id As String, ByVal _factura As String, ByVal _fecha As String)
+
+    Public Sub New(ByVal _id As String, ByVal _factura As String, ByVal _fecha As String, ByVal _iva As String, ByVal _neto As String, ByVal _total As String)
 
         Me._id = _id
         Me._factura = _factura
         Me._fecha = _fecha
+        Me._iva = _iva
+        Me._neto = _neto
+        Me._total = _total
 
     End Sub
 
@@ -43,6 +50,42 @@ Public Class NotaDeCredito
 
         Set(ByVal value As String)
             _fecha = value
+        End Set
+
+    End Property
+
+    Public Property Iva() As String
+
+        Get
+            Return _iva
+        End Get
+
+        Set(ByVal value As String)
+            _iva = value
+        End Set
+
+    End Property
+
+    Public Property Neto() As String
+
+        Get
+            Return _neto
+        End Get
+
+        Set(ByVal value As String)
+            _neto = value
+        End Set
+
+    End Property
+
+    Public Property Total() As String
+
+        Get
+            Return _total
+        End Get
+
+        Set(ByVal value As String)
+            _total = value
         End Set
 
     End Property
